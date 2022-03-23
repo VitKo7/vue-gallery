3430c13a8c15042f7d9d9eb8252b0336bb962f1c
Vitaliy Kornienko <Kornienko.Vitaliy@gmail.com>
2022-03-23 23:51:13 +0200

    added photos on a PhotoPage

--- src/pages/PhotoPage.vue
+++ src/pages/PhotoPage.vue
@@ -1,9 +1,29 @@
 <template>
-    <div>PHOTOS</div>
+    <v-container>
+        <PhotoGallery
+            v-for="photo in photos"
+            v-bind:photo="photo"
+            :key="photo.id"
+        />
+    </v-container>
 </template>
 
 <script>
-export default {};
+import PhotoGallery from '../components/photo/PhotoGallery';
+
+export default {
+    data: () => ({
+        photos: [
+            { id: 1, title: 'фото 1' },
+            { id: 2, title: 'фото 2' },
+            { id: 3, title: 'фото 3' },
+            { id: 4, title: 'фото 4' },
+        ],
+    }),
+    components: {
+        PhotoGallery,
+    },
+};
 </script>
 
 <style lang="scss" scoped></style>
