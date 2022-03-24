3da8fc12c9a4df44015acdb072a6cce582ea3179
Vitaliy Kornienko <Kornienko.Vitaliy@gmail.com>
2022-03-24 22:25:07 +0200

    add axios

--- src/pages/PhotoPage.vue
+++ src/pages/PhotoPage.vue
@@ -13,12 +13,17 @@ import PhotoGallery from '../components/photo/PhotoGallery';
 
 export default {
     data: () => ({
-        photos: [
-            { id: 1, title: 'фото 1' },
-            { id: 2, title: 'фото 2' },
-            { id: 3, title: 'фото 3' },
-            { id: 4, title: 'фото 4' },
-        ],
+        // photos: [
+        //     { id: 1, title: 'фото 1' },
+        //     { id: 2, title: 'фото 2' },
+        //     { id: 3, title: 'фото 3' },
+        //     { id: 4, title: 'фото 4' },
+        // ],
+        photos: [],
+        mounted() {
+            // async request for photos when component is made up;
+            this.axios.get();
+        },
     }),
     components: {
         PhotoGallery,
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import MainPage from './pages/MainPage.vue';
+import MainPage from '@/pages/MainPage.vue';
 import HomePage from './pages/HomePage.vue';
 
 Vue.use(VueRouter);
